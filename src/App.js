2c2376f610667494c48f7492c3d5ffad6d673e60
BigilAnkit <gehlotankit19@gmail.com>
2024-02-04 00:34:24 +0530

    learn props child props jsondata

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
 import './App.css';
 import Footer from './Footer';
 import Header from './Header';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+// import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import { blog } from './Data/blog';
+
+
 
 function App() {
+  let headerInfo={
+    email:"wondercement@gmail.com",
+    phone:"38293843223"
+  }
   return (
     <div className="App">
-      <Header/>
+      {/* <Header email='ankitgehlot' phoneNumberinWords="zeroonezeroonejablagojabband"/> */}
+      {/* props d-structuring */}
+      {/* <Header headerInfo={headerInfo}/> */}
+      
+      <Header headerInfo={headerInfo}>
+        {/* child props */}
+        <h1>Welcome to Ankit Full Stack journiy</h1>
+      </Header>
       <Footer/>
-      <Card/>
+      <div className='text-danger'>
+        HEllo Ankit
+      </div>
+      <Container>
+        <Row>
+          { blog.map((v,i)=>{
+            return (
+              <ProductItems pitems={v}  key={i}/>
+            )
+          })}
+        
+          
+        </Row>
+      </Container>
     </div>
   );
 }
@@ -15,8 +47,23 @@ function App() {
 export default App;
 
 
-function Card(){
+function ProductItems({pitems}){
   return(
-    <h1 className='cardItems'>Card is here</h1>
-  );
+    <>
+  <div className='col-lg-3 mb-4'>  <Card style={{ width: '18rem' }}>
+      {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+      <Card.Body>
+        <Card.Title>
+          {pitems.title}
+        </Card.Title>
+        <Card.Text>
+         {pitems.body}
+        </Card.Text>
+        <Button variant="primary">{pitems.id}</Button>
+      </Card.Body>
+    </Card> 
+    </div>
+
+    </>
+  )
 }
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 
-const Header = () => {
+const Header = ({props,children}) => {
+  
+    // console.log(props.headerInfo.email); 
   return (
-    <div>Header</div>
+    <div>Header
+      {/* <h1>Welcome Props {props.email} {props.phoneNumberinWords}</h1> */}
+      
+      {/* de-Structuring */}
+      <h1>Welcome </h1>
+      {children}
+      
+
+    </div>
   )
 }
 
